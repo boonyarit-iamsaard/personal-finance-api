599da3fb1bfd258675966917f14a381b732dbe4d
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-12-07 16:33:10 +0700

    build: initialize project

---
+++ src/test/java/me/boonyarit/finance/ApplicationTests.java
@@ -0,0 +1,12 @@
+package me.boonyarit.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
