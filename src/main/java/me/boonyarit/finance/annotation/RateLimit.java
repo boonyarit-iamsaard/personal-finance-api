99fe5e5706c4365936b9739dbce4ddf4fc9e4deb
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-12-30 22:35:35 +0700

    feat(security): [WIP] add rate limiting

---
+++ src/main/java/me/boonyarit/finance/annotation/RateLimit.java
@@ -0,0 +1,17 @@
+package me.boonyarit.finance.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimit {
+    String key() default "general";
+
+    long capacity();
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+}
